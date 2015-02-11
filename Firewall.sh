627d61e8fade4cc08742527de20f93cdbe664ec9
Thilina Ratnayake <thilinaratnayake@Thilinas-MacBook-Air.local>
2015-02-10 18:16:31 -0800

    Stuff to comit for elton

--- Firewall.sh
+++ Firewall.sh
@@ -2,18 +2,14 @@
 
 IPT="/sbin/iptables"
 
-
-
 ####USER CONFIG SECTION ###
-IPfw="192.168.0.14"
-IPhost="192.168.10.2"
+firewallHost="192.168.10.1"
+internalHost="192.168.10.2"
 intOut="em1"
 intIn="p3p1"
-#HTTP + HTTPS
-TCPallow="80,443,22"
-#DNS + DHCP
-UDPallow="53,67,68"
-ICMPallow=( 0 8 )
+TCPallow="80,443,5000"
+UDPallow="53,67,68,80,500"
+ICMPallow=( 0 3 8 )
 
 #### END USER CONFIG SECTION ###
 
@@ -25,8 +21,6 @@ $IPT -F
 $IPT -P INPUT DROP -m comment --comment "Default policy INPUT DROP"
 $IPT -P FORWARD DROP -m comment --comment "Default policy FORWARD DROP"
 $IPT -P OUTPUT DROP -m comment --comment "Default policy OUTPUT DROP"
-
-
 
 #Allow DNS for FIREWALLHOST
 	#Allow going OUT to port 53 (initiating a DNS request)
@@ -43,87 +37,65 @@ $IPT -P OUTPUT DROP -m comment --comment "Default policy OUTPUT DROP"
 	$IPT -A OUTPUT -p TCP --dport 68 -j ACCEPT
 	$IPT -A INPUT -p TCP --sport 68 -j  ACCEPT
 
+#Drop all packets destined for FIREWALL from the outside
+	$IPT -A INPUT -i $intOut -d $firewallHost -j DROP -m comment --comment "Drop all packets destined for firewall from outside"
 
+#Block all packets with a source address from outside that has a source address of internal network
+	$IPT -A FORWARD -s 192.168.10.0/24 -i $intOut -j DROP -m comment --comment "Block packet with same source add as internal network"
 
-#Block ALL TCP packets with SYN-FIN bits set (TESTING DOESNT WORK)
-	##$IPT -A FORWARD -p TCP -i $intOut -o $intIn --tcp-flags SYN,FIN SYN,FIN -j DROP -m comment --comment "Block SYNFIN"
-
-#Block all packets with a source address from outside that has a source address of internal network **HAS TO BE BELOW SYN/FIN STUFF **
-	##$IPT -A FORWARD -s 192.168.10.0/255.255.255.0 -j DROP
+#Block ALL TCP packets with SYN-FIN bits set
+	$IPT -A FORWARD -p TCP -i $intOut -o $intIn --tcp-flags SYN,FIN SYN,FIN -j DROP -m comment --comment "Do not allow SYN-FIN"
 
 #Block all external traffic directed to custom ports
 	$IPT -A FORWARD -p tcp -i $intOut -o $intIn --dport 32768:32775 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 32768-32775" 
-	##$IPT -A FORWARD -p tcp -i $intOut -o $intIn --dport 137:139 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 137-139" 
-	##$IPT -A FORWARD -p tcp -i $intOut -o $intIn -m multiport --dport 111,515 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 111,515" 
-	##$IPT -A FORWARD -p udp -i $intOut -o $intIn --dport 32768:32775 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 32768-32775"
-	##$IPT -A FORWARD -p udp -i $intOut -o $intIn --dport 137:139 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 32768-32775"
-	##$IPT -A FORWARD -p udp -i $intOut -o $intIn -m multiport --dport 111,515 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 32768-32775"
-	#$IPT -A FORWARD -p udplite -i $intOut -o $intIn -m multiport --dport 32768:32775 -j DROP
-	#$IPT -A FORWARD -p udplite -i $intOut -o $intIn -m multiport --dport 137:139 -j DROP
-	#$IPT -A FORWARD -p udplite -i $intOut -o $intIn -m multiport --dport 111,515 -j DROP
-	#$IPT -A FORWARD -p sctp -i $intOut -o $intIn --dport 32768:32775 -j DROP
-	#$IPT -A FORWARD -p sctp -i $intOut -o $intIn --dport 137:139 -j DROP
-	#$IPT -A FORWARD -p sctp -i $intOut -o $intIn -m multiport --dport 111,515 -j DROP
-	#$IPT -A FORWARD -p dccp -i $intOut -o $intIn --dport 32768:32775 -j DROP
-	#$IPT -A FORWARD -p dccp -i $intOut -o $intIn --dport 137:139 -j DROP
-	#$IPT -A FORWARD -p dccp -i $intOut -o $intIn -m multiport --dport 111,515 -j DROP
+	$IPT -A FORWARD -p tcp -i $intOut -o $intIn --dport 137:139 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 137-139" 
+	$IPT -A FORWARD -p tcp -i $intOut -o $intIn -m multiport --dport 111,515 -j DROP -m comment --comment "DROP ALL TCP TRAFFIC TO 111,515" 
+	$IPT -A FORWARD -p udp -i $intOut -o $intIn --dport 32768:32775 -j DROP -m comment --comment "DROP ALL UDP TRAFFIC TO 32768-32775"
+	$IPT -A FORWARD -p udp -i $intOut -o $intIn --dport 137:139 -j DROP -m comment --comment "DROP ALL UDP TRAFFIC TO 32768-32775"
 
-#Block all SYN TO high ports
-	##$IPT -A FORWARD -p TCP --tcp-flags SYN SYN ! --dport 1:1024 -j DROP
 
 #Block all TELNET
-	##$IPT -A FORWARD -p TCP --sport 23 -j DROP  -m comment --comment "DROP ALL INCOMING TELNET TCP"
-	##$IPT -A FORWARD -p TCP --dport 23 -j DROP  -m comment --comment "DROP ALL INCOMING TELNET TCP"
-	##$IPT -A FORWARD -p UDP --sport 23 -j DROP  -m comment --comment "DROP ALL INCOMING TELNET UDP"
-	##$IPT -A FORWARD -p UDP --dport 23 -j DROP  -m comment --comment "DROP ALL INCOMING TELNET UDP"
-
-#Drop all packets destined for FIREWALL from the outside
-	#$IPT -A INPUT -i em1 -j DROP -m comment --comment "Drop all packets destined for firewall from outside"
+	$IPT -A FORWARD -p TCP --sport 23 -j DROP -m comment --comment "DROP ALL TELNET TCP"
+	$IPT -A FORWARD -p TCP --dport 23 -j DROP -m comment --comment "DROP ALL TELNET TCP"
 
 #Accept Fragments (TESTING DOESNT WORK)
-	##$IPT -A FORWARD -f -j ACCEPT
-
-
-
-#Custom SSH and FTP rules
-##$IPT -A PREROUTING -t mangle -p tcp --sport 22 \
-  #-j TOS --set-tos Minimize-Delay
-##$IPT -A PREROUTING -t mangle -p tcp --sport 21 \
-  #-j TOS --set-tos Minimize-Delay
-##$IPT -A PREROUTING -t mangle -p tcp --sport 20 \
-  #-j TOS --set-tos Maximize-Throughput
+	$IPT -A FORWARD -f -j ACCEPT -m state --state new,established
 
 #Accept all inbound/outbound TCP on custom ports
-	##$IPT -A FORWARD -p TCP -m multiport --dport $TCPallow -j ACCEPT
-	##$IPT -A FORWARD -p TCP -m multiport --sport $TCPallow -j ACCEPT
+	$IPT -A FORWARD -p TCP -m multiport --dport $TCPallow -j ACCEPT -m state --state new,established
+	$IPT -A FORWARD -p TCP -m multiport --sport $TCPallow -j ACCEPT -m state --state new,established
 
 #Accept all inbound/outbound UDP on custom ports
-	##$IPT -A FORWARD -p UDP -m multiport --dport $UDPallow -j ACCEPT
-	##$IPT -A FORWARD -p UDP -m multiport --sport $UDPallow -j ACCEPT
+	$IPT -A FORWARD -p UDP -m multiport --dport $UDPallow -j ACCEPT
+	$IPT -A FORWARD -p UDP -m multiport --sport $UDPallow -j ACCEPT
+
+#Allow ports 20,21,22 for SSH/FTP rules
+	$IPT -A FORWARD -p TCP -m multiport --dport 20,21,22 -j ACCEPT -m state --state new,established
+	$IPT -A FORWARD -p TCP -m multiport --sport 20,21,22 -j ACCEPT -m state --state new,established
+
+#Custom SSH and FTP rules
+$IPT -A PREROUTING -t mangle -p tcp --sport 22 \
+  -j TOS --set-tos Minimize-Delay
+$IPT -A PREROUTING -t mangle -p tcp --sport 21 \
+  -j TOS --set-tos Minimize-Delay
+$IPT -A PREROUTING -t mangle -p tcp --sport 20 \
+  -j TOS --set-tos Maximize-Throughput
 
 #Accept all ICMP on custom packet types
 
-#for i in "${ICMPallow[@]}"
-#do
-#	:	
-#	$IPT -A FORWARD -p ICMP --icmp-type $i -j ACCEPT
-#	echo "done $i"
-		
-#
-$IPT -A FORWARD -p ICMP --icmp-type 0 -j ACCEPT
-$IPT -A FORWARD -p ICMP --icmp-type 8  -j ACCEPT
+for i in "${ICMPallow[@]}"
+do
+	:	
+	$IPT -A FORWARD -p ICMP -i $intOut -o $intIn --icmp-type $i -j ACCEPT -m comment --comment "ALLOW ICMP"
+	$IPT -A FORWARD -p ICMP -i $intIn -o $intOut --icmp-type $i -j ACCEPT -m comment --comment "ALLOW ICMP"
+	echo "done $i"	
+done
 
-
-
-#Accept ALL TCP Segments that belong to an existing connection
-	##$IPT -A FORWARD -p TCP -m state --state established -j ACCEPT
-
-
+#Block all SYN TO high ports
+	$IPT -A FORWARD -p TCP --tcp-flags SYN SYN ! --dport 1:1024 -j DROP
 
 
 echo "New IPTABLES SET";
-
-
 service iptables save
 service iptables restart
 
